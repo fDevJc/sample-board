b153503796b4ed05ea884aabbecb4736606b749e
fDevJc <fdevjc@gmail.com>
2023-02-16 14:59:09 +0900

    init commit

---
+++ src/main/java/com/jc/callbustask/CallbusTaskApplication.java
@@ -0,0 +1,13 @@
+package com.jc.callbustask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallbusTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallbusTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jc/callbustask/CallbusTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.callbustask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallbusTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
