8f83cae365346dc080368052416e98ad1758659f
fDevJc <fdevjc@gmail.com>
2023-02-16 15:39:44 +0900

    feat: add JpaAuditing annotation

--- src/main/java/com/jc/callbustask/CallbusTaskApplication.java
+++ src/main/java/com/jc/callbustask/CallbusTaskApplication.java
@@ -2,7 +2,9 @@ package com.jc.callbustask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CallbusTaskApplication {
 
